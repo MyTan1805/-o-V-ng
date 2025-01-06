4e5c45eaea901add211ed6af9c322383b1b62b72
MyTan1805 <165132433+MyTan1805@users.noreply.github.com>
2025-01-06 15:57:06 +0700

    'DaoVang'

---
+++ Assets/Scripts/Helper Scripts/TagManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Tags
+{
+    public const string LARGE_GOLD = "Big Gold";
+    public const string MIDDLE_GOLD  = "Middle Gold";
+    public const string SMALL_GOLD = "small gold";
+    public const string MIDDLE_STONE = "Middle Stone";
+    public const string LARGE_STONE = "Big Stone";
+    public const string Small_Stone = "Small Stone";
+    public const string Diamond = "Diamond";
+    public const string DELIVER_ITEM = "DELIVER_ITEM";
+}
+
+public class AnimationTags
+{
+    public const string IDLE_ANIMATION = "idle";
+    public const string Chear_animation = "Chear";
+    public const string ROPE_WRAP_ANIMATION = "Rope Wrap";
+
+}
